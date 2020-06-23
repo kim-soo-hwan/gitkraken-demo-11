d1f7934ca2eed6c85d959cb82b1db0668ddde990
Soohwan Kim <kimsoohwan@sunmoon.ac.kr>
2020-06-23 15:15:37 +0900

    Create the game structure

---
+++ rockpapaerscissors.py
@@ -0,0 +1,9 @@
+# list of play options
+play = ["Rock", "Paper", "Scissors"]
+
+# assign a random play to the computer
+computer = "Rock"
+print('Computer: {}'.format(computer))
+
+# get the user input
+user = "Paper"
